ee635844d23d6dfd289124b6969a41775f6ee019
amaben2020 <amaben202phantom@gmail.com>
2024-09-28 21:00:28 +0100

    updates

--- packages/functions/src/create.ts
+++ packages/functions/src/create.ts
@@ -1,48 +1,30 @@
 import * as uuid from 'uuid';
 import { Resource } from 'sst';
-import { APIGatewayProxyEvent } from 'aws-lambda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { PutCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { Util } from '@monorepo-template/core/util';
 
 const dynamoDb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
-export async function main(event: APIGatewayProxyEvent) {
-  let data, params;
-  // Request body is passed in as a JSON encoded string in 'event.body'
-  if (event.body) {
+
+export const main = Util.handler(async (event) => {
+  let data = {
+    content: '',
+    attachment: '',
+  };
+  if (event.body != null) {
     data = JSON.parse(event.body);
-    params = {
-      TableName: Resource.Notes.name,
-      Item: {
-        // The attributes of the item to be created
-        userId: '123', // The id of the author
-        noteId: uuid.v1(), // A unique uuid
-        content: data.content, // Parsed from request body
-        attachment: data.attachment, // Parsed from request body
-        createdAt: Date.now(), // Current Unix timestamp
-      },
-    };
-  } else {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ error: true }),
-    };
   }
-  try {
-    await dynamoDb.send(new PutCommand(params));
-    return {
-      statusCode: 201,
-      body: JSON.stringify(params.Item),
-    };
-  } catch (error) {
-    let message;
-    if (error instanceof Error) {
-      message = error.message;
-    } else {
-      message = String(error);
-    }
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: message }),
-    };
-  }
-}
+  const params = {
+    TableName: Resource.Notes.name,
+    Item: {
+      // The attributes of the item to be created
+      userId: '123', // The id of the author
+      noteId: uuid.v1(), // A unique uuid
+      content: data.content, // Parsed from request body
+      attachment: data.attachment, // Parsed from request body
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+  await dynamoDb.send(new PutCommand(params));
+  return JSON.stringify(params.Item);
+});
